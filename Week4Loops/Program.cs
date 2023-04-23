0440aa6a51a7346e66f719e28a949ef2f9449c87
MarkoLauer <marko.lauer@tktk.ee>
2023-04-23 21:26:09 +0300

    add week4 Loops

---
+++ Week4Loops/Program.cs
@@ -0,0 +1,10 @@
+﻿
+//for-loop
+//DRY principle - Do not repeat yourself
+
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine("We don't need no education");
+}
+
+Console.WriteLine("Have a nice day!");
---
+++ WhileLoops/Program.cs
@@ -0,0 +1,37 @@
+﻿
+
+using System.ComponentModel.Design;
+
+//Random rnd = new Random();
+//int cpuRandom;
+
+//bool loopActive = true; //bool - true/false
+
+//while (loopActive)
+//{
+//    cpuRandom = rnd.Next(1, 4);
+//    Console.WriteLine($"cpu created rnd {cpuRandom}");
+//    Console.WriteLine("Make a guess. Enter a number 1-3");
+//    int userNumber = Int32.Parse(Console.ReadLine());
+
+//    if (userNumber == cpuRandom) 
+//    {
+//        Console.WriteLine("Congratulations, you won!");
+//        loopActive = false;
+//        //"break" lõpetab while loopimise jõuga
+//    } 
+//    else
+//    {
+//        Console.WriteLine("Oops, try again!");
+//    }
+//}
+//
+//Console.WriteLine("Have a nice day!");
+
+
+Console.WriteLine("Enter a number");
+int userNumber = Int32.Parse(Console.ReadLine());
+for (int i = 0; i < 10; i++)
+{
+    Console.WriteLine($"{i + 1} * {userNumber} = {(i +1) * userNumber}");
+}
